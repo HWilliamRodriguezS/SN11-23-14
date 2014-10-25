6b6db2382b512aa194f4a62e0acb07a1614e1abb
HWilliamRS <HWilliamRS@Gmail.com>
2014-10-25 15:40:41 -0400

    Create Project (Not Functional) {0.25h} Create Repository and Initialize(Not Functional){0.5h}

---
+++ application/controllers/site.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of site
+ *
+ * @author William
+ */
+class site {
+    //put your code here
+    
+    
+    public function index()
+	{
+		echo "Let's The Project Begin!";
+	}
+}
